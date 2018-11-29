ec355aaa65a9e7fe233610eca4c584a37195f1ec
lantcl <cleolant@gmail.com>
2018-11-29 16:52:34 -0500

    js update

--- js/script.js
+++ js/script.js
@@ -27,6 +27,33 @@ if (window.matchMedia("(max-width: 480px)").matches) {
             castspell.style.display = "block";
         };
 
+    var spell = document.getElementsByClassName("spell");
+
+        spell.addEventListener("touchstart", playsparkle, false);
+
+        function playsparkle(e){
+            var sound = new sound("assets/sparkle.mp3");
+
+        function sound(src) {
+            this.sound = document.createElement("audio");
+            this.sound.src = src;
+            this.sound.setAttribute("preload", "auto");
+            this.sound.setAttribute("controls", "none");
+            this.sound.style.display = "none";
+            document.body.appendChild(this.sound);
+            this.play = function(){
+                this.sound.play();
+            }
+            this.stop = function(){
+                this.sound.pause();
+            }
+        }
+            sound.play();
+            document.getElementsByClassName("spellButton");
+            spellButton.style.display = "block";
+        };
+
+
 
         var avatar = document.getElementById("avatar");
 
@@ -78,6 +105,33 @@ if (window.matchMedia("(max-width: 480px)").matches) {
         	sound.play();
             document.getElementById("castspell");
             castspell.style.display = "block";
+        };
+
+
+    var spell = document.getElementsByClassName("spell");
+
+        spell.addEventListener("click", playsparkle, false);
+
+        function playsparkle(e){
+            var sound = new sound("assets/sparkle.mp3");
+
+        function sound(src) {
+            this.sound = document.createElement("audio");
+            this.sound.src = src;
+            this.sound.setAttribute("preload", "auto");
+            this.sound.setAttribute("controls", "none");
+            this.sound.style.display = "none";
+            document.body.appendChild(this.sound);
+            this.play = function(){
+                this.sound.play();
+            }
+            this.stop = function(){
+                this.sound.pause();
+            }
+        }
+            sound.play();
+            document.getElementsByClassName("spellButton");
+            spellButton.style.display = "block";
         };
 
 
