effa1f7e7e90c9febe11baf1a7644ec4adfd622f
lantcl <cleolant@gmail.com>
2018-11-29 16:27:10 -0500

    mobile js

---
+++ js/mobile-script.js
@@ -0,0 +1,52 @@
+console.log("connected");
+
+
+var spellBtn = document.getElementsByClassName("spellBtn");
+
+spellBtn.addEventListener("touchstart", playsparkle, false);
+
+function playsparkle(e){
+	var sound = new sound("assets/sparkle.mp3");
+
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+    }
+}
+	sound.play();
+};
+
+
+
+var avatar = document.getElementById("avatar");
+
+avatar.addEventListener("touchstart", playshine, false);
+
+function playshine(e){
+var sound = new sound("assets/shine.mp3");
+
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+    }
+}
+sound.play();
+};
