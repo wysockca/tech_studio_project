e8649b51751b37eabda6d87c6f6aca2b2ec87187
lantcl <cleolant@gmail.com>
2018-11-29 15:41:24 -0500

    winner dashboard

---
+++ win-dashboard.php
@@ -0,0 +1,71 @@
+<?php 
+
+session_start();
+
+if($_SESSION['logged-in'] == false){
+	header("Location: main.php");
+}else{
+
+$dsn = "mysql:host=localhost;dbname=wysockca_spellbook;charset=utf8mb4";
+$dbusername = "wysockca";
+$dbpassword = "sxRaM*y74c4";
+
+$pdo = new PDO($dsn, $dbusername, $dbpassword); 
+
+$id = $_SESSION['id'];
+
+$stmt = $pdo->prepare("SELECT * FROM `userinfo` WHERE `id` = '$id'");
+$stmt->execute();
+$row = $stmt->fetch();
+//selecting all information about the user based on their id
+
+$level = $row["level_id"];
+
+$stmt3 = $pdo->prepare("SELECT * FROM `levels` WHERE `id` = '$level'");
+$stmt3->execute();
+$row3 = $stmt3->fetch();
+//selecting the level information based on the users level
+
+$stmt4 = $pdo->prepare("SELECT * FROM `levels-tower` WHERE `levelid` = '$level'");
+$stmt4->execute();
+$row4 = $stmt4->fetch();
+//selecting the tower based on the users level
+
+?>
+
+<!doctype html>
+<html>
+	<head>
+		<title>Spellbound: Enchanted Ascent</title>
+		<meta charset="utf-8" />
+		<meta content="width=device-width, initial-scale=1" name="viewport" />
+
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<link rel="stylesheet" media="screen and (max-width: 480px)" href="css/small.css" /> 
+		<link rel="stylesheet" type="text/css" href="css/winner.css"> 
+	</head>
+	<body>
+	<header>
+			<a id="logo" href="dashboard.php"><img src="assets/logo.svg" /></a>
+			<nav>
+				<div class="flex-container">
+	            	<a class ="button" href="edit-profile.php"><img src="assets/editprofilebutton.svg" /></a>          	
+	            	<a class ="button" href="logout.php"><img src="assets/logoutbutton.svg" /></a>
+            	</div> 
+			</nav>
+		</header>
+		<div id="userinfo">
+			<h1><?php echo($row["username"]); ?></h1>
+			<h2>Current Level: <?php echo($row3["level"]); ?></h2>
+			<h2>Congratulations! You have mastered magic!</h2>
+		</div>
+		<div id="moon"><img src="assets/moon.svg" /></div>
+		<section>
+			<div id="tower"><img src="assets/<?php echo($row4["image"]); ?>" /></div>
+			<div id="avatar"><img id="avatar" src="assets/<?php echo($row3["image"]); ?>" /></div>
+		</section>
+		<a class="link" id="sb-link" href="spellbook.php"><img src="assets/spellbookbutton2.svg" /></a>
+	</body>
+</html>
+
+<?php } ?>
